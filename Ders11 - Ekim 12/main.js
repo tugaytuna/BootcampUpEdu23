2497c11affbb07f6e9048004b59cf261d5502b0f
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-10-12 21:35:42 +0300

    ders11 added

---
+++ Ders11 - Ekim 12/main.js
@@ -0,0 +1,17 @@
+var isim = "tugay";
+var yas = 18;
+
+let isim2 = "esma";
+let yas2 = 23;
+
+yas = 20;
+
+console.log("merhaba " + isim);
+
+console.log(yas + " yaşındayım");
+
+yas = 40;
+
+console.log(yas + " yaşındayım");
+
+// console.log(yas * yas);
---
+++ Ders11 - Ekim 12/main2.js
@@ -0,0 +1,24 @@
+let name = "tugay";
+let surname = "tuna";
+
+let age = 18;
+let age2 = 25;
+
+console.log("hoşgeldin " + name + " " + surname);
+
+console.log(age);
+
+age = age + age2;
+
+age2 = 20;
+
+age = age + age2;
+
+console.log(age);
+console.log(age2);
+
+// ------console
+// hoşgeldin tugay tuna
+// 18
+// 63
+// 20

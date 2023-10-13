304788bffefaf7c5c3a623c113fe68b352b0c341
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-10-13 21:43:31 +0300

    ders12 added

---
+++ Ders12 - Ekim 13/kod.js
@@ -0,0 +1,33 @@
+let name = "esma";
+let surname = "tuna";
+
+let fullName;
+
+let age = 10;
+let isAdmin = true;
+let isValid = false;
+
+fullName = name + " " + surname;
+
+name = "esma";
+surname = "yılmaz";
+
+console.log(fullName);
+console.log(name);
+
+age = age + 2;
+
+// let con1 = age > 18;
+// console.log(con1);
+
+// if (age >= 18) {
+//   console.log("sitemize girebilirsiniz");
+// } else {
+//   console.log("yaşınız 18'den küçüktür, siteye giremezsiniz!");
+// }
+
+if (age >= 18 && name == "tugay") {
+  console.log("hoşgeldin siteye giriş yapabilirsiniz");
+} else {
+  console.log("siteye giriş yapamazsınız!");
+}

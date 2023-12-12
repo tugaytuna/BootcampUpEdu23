236b8f07aed880dfe99ecb754786afc074885ffa
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-12-12 21:32:31 +0300

    ders34 added

--- Ders34 - 12 Aralık/useeffect/src/App.js
+++ Ders34 - 12 Aralık/useeffect/src/App.js
@@ -1,23 +1,40 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [input1, setInput1] = useState("");
+  const [validText, setValidText] = useState(false);
+
+  useEffect(() => {
+    setValidText(false);
+    if ((input1.length > 3 && !input1.includes(" ")) || input1 == "") {
+      setValidText(true);
+    }
+  }, [input1]);
+
+  function signUp() {
+    console.log(input1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>useEffect Kullanımı</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+        eius quidem numquam exercitationem mollitia inventore veniam est vitae
+        harum laboriosam quis necessitatibus optio totam maiores rem nam
+        ducimus, repudiandae ratione.
+      </p>
+      <input
+        className={validText ? "valid" : "notvalid"}
+        value={input1}
+        onChange={(e) => {
+          setInput1(e.target.value);
+        }}
+        type="text"
+      />
+      <br />
+      <button onClick={signUp}>Sign Up</button>
     </div>
   );
 }

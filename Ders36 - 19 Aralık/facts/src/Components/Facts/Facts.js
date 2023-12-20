f12691d4b6dc8ea0b7d76845c7f809e9ae5ca7f7
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-12-20 21:13:15 +0300

    half-way-point

--- Ders36 - 19 Aralık/facts/src/Components/Facts/Facts.js
+++ Ders36 - 19 Aralık/facts/src/Components/Facts/Facts.js
@@ -4,23 +4,64 @@ import axios from "axios";
 
 function Facts() {
   const [fact, setFact] = useState("");
-  const API_URL = "https://uselessfacts.jsph.pl/api/v2/facts/random";
+  const [err, setErr] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [lang, setLang] = useState("en");
+
+  const API_URL = "https://uselessfacts.jsph.pl/api/v2/facts/random?language=";
   //const API_URL_EN = "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en";
   //const API_URL_DE = "https://uselessfacts.jsph.pl/api/v2/facts/random?language=de";
 
   function getFacts() {
-    axios.get(API_URL).then((data) => {
-      //   console.log(data.data);
-      setFact(data.data);
-    });
+    setLoading(true);
+    axios
+      .get(API_URL + lang)
+      .then((data) => {
+        //   console.log(data.data);
+        setErr(false);
+        setFact(data.data);
+      })
+      .catch((err) => {
+        setErr(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }
 
   return (
     <div>
+      <label htmlFor="eng">English</label>
+      <input
+        type="radio"
+        name="lan"
+        id="eng"
+        onChange={() => {
+          setLang("en");
+        }}
+      />
+      <label htmlFor="de">German</label>
+      <input
+        type="radio"
+        name="lan"
+        id="de"
+        onChange={(e) => {
+          setLang("de");
+        }}
+      />
+      <br />
+      <br />
       <button onClick={getFacts} className="facts-button1">
         Get New Facts
       </button>
-      <p className="Pfacts">{fact.text}</p>
+      <p className="Pfacts">{loading ? "Loading..." : fact.text}</p>
+      {!err || (
+        <h4 style={{ color: "#B80000" }}>
+          There is an error! Please try again and check your connection.
+        </h4>
+      )}
+      {/* <h6>{fact.source}</h6> */}
     </div>
   );
 }

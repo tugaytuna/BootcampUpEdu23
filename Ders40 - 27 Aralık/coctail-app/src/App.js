735ecd02058d6bdee6318073fbbefdf7be3fd0d2
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-12-27 21:55:15 +0300

    ders40 added

--- Ders40 - 27 Aralık/coctail-app/src/App.js
+++ Ders40 - 27 Aralık/coctail-app/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [allCoctails, setAllCoctails] = useState([]);
+  const [inputSearch, setInputSearch] = useState("");
+
+  const API_ListAllLetter =
+    "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=c";
+
+  const API_SearchbyName =
+    "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+  function getAllCoctails() {
+    axios.get(API_SearchbyName + inputSearch).then((response) => {
+      console.log(response.data.drinks);
+      setAllCoctails(response.data.drinks);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Coctail App</h1>
+      <input
+        className="searchCoctail"
+        placeholder="write a coctail..."
+        type="text"
+        name=""
+        id=""
+        value={inputSearch}
+        onChange={(e) => {
+          setInputSearch(e.target.value);
+        }}
+      />
+      <br />
+      <button className="searchButton" onClick={getAllCoctails}>
+        Search
+      </button>
+
+      {allCoctails.map((item) => {
+        if (item.strAlcoholic == "Alcoholic") {
+          return (
+            <div>
+              <h6>{item.strDrink}</h6>
+              <label htmlFor="">{item.strAlcoholic}</label>
+              <img height={100} width={100} src={item.strDrinkThumb} alt="" />
+            </div>
+          );
+        }
+      })}
     </div>
   );
 }
